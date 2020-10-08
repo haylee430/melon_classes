12d53e9b823de4402f2070bc62107d5630ab3d7c
Haylee Thomas-Kuhlmann <hay.design.lee@gmail.com>
2020-10-08 16:38:25 -0500

    Lab Completed

--- harvest.py
+++ harvest.py
@@ -21,7 +21,7 @@ class MelonType(object):
     def add_pairing(self, pairing):
         """Add a food pairing to the instance's pairings list."""
 
-        self.pairings = self.pairings.append(pairing)
+        self.pairings.extend(pairing)
 
     def update_code(self, new_code):
         """Replace the reporting code with the new_code."""
@@ -38,21 +38,36 @@ def make_melon_types():
     crenshaw_melon = MelonType('cren','1996', 'green', True, False, "Crenshaw Melon")
     yellow_water_melon = MelonType('yw','2013', 'yellow', False, True, "Yellow Watermelon")
 
-    all_melon_types.append(muck_melon.name , casaba_melon.name, crenshaw_melon.name, yellow_water_melon.name)
+    musk_melon.add_pairing(["mint"])
+    casaba_melon.add_pairing(["strawberries","mint"])
+    crenshaw_melon.add_pairing(["proscuitto"])
+    yellow_water_melon.add_pairing(["ice cream"])
 
-    print(all_melon_types)
+    all_melon_types.extend([musk_melon, casaba_melon, crenshaw_melon, yellow_water_melon])
 
     return all_melon_types
 
-def print_pairing_info(melon_types):
+def print_pairing_info():
     """Prints information about each melon type's pairings."""
 
-    # Fill in the rest
+    melon_types = make_melon_types()
+    
+    for melon in melon_types:
+        print (f"{melon.name} pairs with")
 
-def make_melon_type_lookup(melon_types):
+        for pair in melon.pairings:
+            print (f"- {pair}")
+ 
+def make_melon_type_lookup():
     """Takes a list of MelonTypes and returns a dictionary of melon type by code."""
 
-    # Fill in the rest
+    melon_types = make_melon_types()
+    melons_dic = {}
+
+    for melon in melon_types:
+        melons_dic[melon.code] = [melon.first_harvest, melon.color, melon.is_seedless, melon.is_bestseller, melon.name, melon.pairings]
+
+    return melons_dic
 
 ############
 # Part 2   #
@@ -61,18 +76,61 @@ def make_melon_type_lookup(melon_types):
 class Melon(object):
     """A melon in a melon harvest."""
 
-    # Fill in the rest
-    # Needs __init__ and is_sellable methods
+    def __init__(self, melon_type, shape_rating, color_rating, field_loc, harvest_human):
+        """Initialize a melon."""
+        self.melon_type = melon_type
+        self.shape_rating = shape_rating
+        self.color_rating = color_rating
+        self.field_loc = field_loc
+        self.harvest_human = harvest_human
 
-def make_melons(melon_types):
+        self.sellable = None
+
+    def is_sellable(self):
+        """Replace the reporting code with the new_code."""
+
+        if self.shape_rating > 5 and self.color_rating > 5 and self.field_loc != 3:
+            self.sellable = True
+        else:
+            self.sellable = False
+
+def make_melons():
     """Returns a list of Melon objects."""
 
-    # Fill in the rest
+    all_melon_types = []
 
-def get_sellability_report(melons):
+    melon_1 = Melon("yw",8,7,2,"Sheila")
+    melon_2 = Melon("yw",3,4,2,"Sheila")
+    melon_3 = Melon("yw",9,8,3,"Sheila")
+    melon_4 = Melon("cas",10,6,35,"Sheila")
+    melon_5 = Melon("cren",8,9,35,"Mike")
+    melon_6 = Melon("cren",8,2,35,"Mike")
+    melon_7 = Melon("cren",2,3,4,"Mike")
+    melon_8 = Melon("musk",6,7,4,"Mike")
+    melon_9 = Melon("yw",7,10,3,"Sheila")
+
+    all_melon_types.extend([melon_1, melon_2, melon_3, melon_4, melon_5, melon_6, melon_7, melon_8, melon_9])
+
+    for melon in all_melon_types:
+        melon.is_sellable()
+    
+    return all_melon_types
+
+def get_sellability_report():
     """Given a list of melon object, prints whether each one is sellable."""
 
-    # Fill in the rest 
+    all_melon_types = make_melons()
+    
+    for melon in all_melon_types:
+        if melon.sellable == True:
+            print (f"Harvested by {melon.harvest_human} from {melon.field_loc} (CAN BE SOLD)")
+        elif melon.sellable == None:
+            print ("It is none")
+        else:
+            print (f"Harvested by {melon.harvest_human} from {melon.field_loc} (NOT SELLABLE)")
+            
+
+
 
 
 
